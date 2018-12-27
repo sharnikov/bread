cd45e2b93b331b47238a6a3b06ba91847fe0e545
o.sharnikov <o.sharnikov@tinkoff.ru>
2018-12-27 11:56:08 +0300

    Generilize some code in services

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,7 +1,9 @@
+import akka.http.scaladsl.marshalling.{ToEntityMarshaller, ToResponseMarshallable, ToResponseMarshaller}
 import akka.http.scaladsl.server.Directives._
 import services.{CatalogService, ServiceException}
 import domain.JsonParsers._
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 class Routes(catalogService: CatalogService) {
@@ -9,19 +11,19 @@ class Routes(catalogService: CatalogService) {
   def getRoutes() =
     get {
       path("all_goods") {
-        onComplete(catalogService.getAllGoods()) {
-          case Success(info) => complete(info)
-          case Failure(exception) => failWith(new ServiceException("Internal exception", exception))
-        }
+        completeResult(catalogService.getAllGoods())
       }
     } ~ get {
       path("goods_by_category") {
         parameters('category.as[String]) { category =>
-          onComplete(catalogService.getGoodsByCategory(category)) {
-            case Success(info) => complete(info)
-            case Failure(exception) => failWith(new ServiceException("Internal exception", exception))
-          }
+          completeResult(catalogService.getGoodsByCategory(category))
         }
       }
+    }
+
+  private def completeResult[T](result: Future[T])(implicit marshaller: ToResponseMarshaller[T]) =
+    onComplete(result) {
+      case Success(info) => complete(info)
+      case Failure(exception) => failWith(new ServiceException("Internal exception", exception))
     }
 }
