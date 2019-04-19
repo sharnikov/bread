27e4d35c31b306889bebfc51000ccc3f654393a1
o.sharnikov <o.sharnikov@tinkoff.ru>
2019-04-19 11:32:18 +0300

    Fix route exceptionHandler

--- src/main/scala/ru/bread/http/routes/RoutesSettings.scala
+++ src/main/scala/ru/bread/http/routes/RoutesSettings.scala
@@ -5,21 +5,21 @@ import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives.complete
 import akka.http.scaladsl.server.{ExceptionHandler, Route}
 import com.typesafe.scalalogging.LazyLogging
-import ru.bread.errors.AppError.{BreadException, VerboseServiceException}
+import ru.bread.errors.AppError.{AuthorizationException, BreadException, VerboseServiceException}
 import ru.bread.errors.ErrorCode
 
 trait RoutesSettings extends LazyLogging {
 
   implicit def exceptionHandler = ExceptionHandler {
+    case exception: AuthorizationException =>
+      logger.error(s"Authorization exception with a message: ${exception.getMessage}", exception)
+      completeWithResponse(Unauthorized, "Login or password is incorrect")
     case exception: VerboseServiceException =>
       logger.error(s"Failed with a verboseException with a message: ${exception.getMessage}", exception)
-      completeWithResponse(BadRequest, s"Failed with a verboseException with a message: ${exception.getMessage}")
+      completeWithResponse(BadRequest, s"${exception.getMessage}")
     case exception: BreadException if exception.code == ErrorCode.DataNotFoundError =>
       logger.error(s"Unable to find a data. Exception message: ${exception.getMessage}", exception)
       completeWithResponse(InternalServerError, "Internal exception")
-    case exception: BreadException if exception.code == ErrorCode.AuthorizationError =>
-      logger.error(s"Authorization exception with a message: ${exception.getMessage}", exception)
-      completeWithResponse(Unauthorized, "Login or password is incorrect")
     case exception =>
       logger.error(s"Failed with an exception with a message: ${exception.getMessage}", exception)
       completeWithResponse(InternalServerError, "Internal exception")
