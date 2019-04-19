6d4241b2c66c993d67778bcd176afc5e348832fb
o.sharnikov <o.sharnikov@tinkoff.ru>
2019-04-19 12:51:59 +0300

    Add comments to db scripts. Add mail to users table

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -1,3 +1,4 @@
+--add goods table
 CREATE TABLE GOODS
 (
   id serial PRIMARY KEY,
@@ -15,6 +16,7 @@ insert into GOODS (name, price, category) values
 select * from GOODS;
 ---------------------------------------
 
+--add users table
 CREATE TABLE USERS
 (
   id serial PRIMARY KEY,
@@ -30,6 +32,7 @@ insert into USERS (login, name, secondname) values
 select * from USERS;
 ----------------------------------------
 
+--add orders table
 CREATE TABLE ORDERS (
     id serial PRIMARY KEY,
     user_id integer references USERS ON DELETE RESTRICT
@@ -40,7 +43,7 @@ insert into ORDERS (user_id) values (1);
 
 select * from ORDERS;
 ------------------------------------
-
+--create items table
 CREATE TABLE ITEMS (
     good_id integer REFERENCES GOODS ON DELETE RESTRICT,
     order_id integer REFERENCES ORDERS ON DELETE CASCADE,
@@ -59,16 +62,19 @@ select * from  ITEMS;
 select g.name, i.quantity from GOODS as g JOIN ITEMS as i on i.good_id = g.id join orders as o on o.id = i.order_id where o.id = 1
 
 ---------------------------
-
+--add types of orders
 create type order_status AS ENUM ('NEW', 'DONE', 'IN_PROGRESS', 'REJECTED', 'WAITING_FOR_CONFIRM');
 alter table ORDERS add column status order_status not null default 'NEW';
 
 --------------------------------------------
+
+--add password to users
 alter table USERS add column password varchar(100) not null default '';
 update USERS set password = 'f6bcd09265d6ca53c14b3ce87d3f177b' where id = 1;
 ALTER TABLE USERS ALTER COLUMN password DROP DEFAULT;
 ----------------------------------
 
+--add roles to users
 create type roles AS ENUM ('ADMIN', 'CLIENT', 'GOD', 'TRUSTED_CLIENT');
 alter table USERS add column role roles not null default 'CLIENT';
 update USERS set role = 'ADMIN' where id = 1;
@@ -76,5 +82,15 @@ insert into USERS (login, name, secondname, password) values
 ('Vovan', 'Вовка', 'Нитуп', 'russia');
 
 -----------------------------------
+--add order creation time
 alter table ORDERS add column creation_date timestamp not null default current_timestamp;
 --------------
+
+--add mail to users
+alter table users add column mail varchar(100) unique;
+select * from USERS;
+update USERS set mail = 'azaz@mail.com' where id = 1;
+update USERS set mail = 'loverussia@mail.com' where id = 3;
+alter table USERS alter column mail set not null;
+
+--------------------------------------------
